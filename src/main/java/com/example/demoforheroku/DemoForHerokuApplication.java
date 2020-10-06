eef8841d7bb1652d31ad9b133cba5b938ac89ab6
Shankar Ghimire <ssghimire@gmail.com>
2020-10-06 00:46:28 -0400

    First commit

---
+++ src/main/java/com/example/demoforheroku/DemoForHerokuApplication.java
@@ -0,0 +1,13 @@
+package com.example.demoforheroku;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoForHerokuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoForHerokuApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/demoforheroku/DemoForHerokuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demoforheroku;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoForHerokuApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
